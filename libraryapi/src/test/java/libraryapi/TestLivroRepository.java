2a807e89e4b20ceee1ad05824e9b647c4b2a729f
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-07-18 16:14:31 -0300

    salvando Testes

---
+++ libraryapi/src/test/java/libraryapi/TestLivroRepository.java
@@ -0,0 +1,71 @@
+package libraryapi;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import libraryapi.enums.GeneroLivro;
+import libraryapi.model.Autor;
+import libraryapi.model.Livro;
+import libraryapi.repository.AutorRepository;
+import libraryapi.repository.LivroRepository;
+
+@SpringBootTest
+public class TestLivroRepository {
+	@Autowired
+	private LivroRepository livroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
+	
+	@Test
+	public void testSalvarLivro() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2014, Calendar.AUGUST, 17);
+		Date datapublicacao = cal.getTime();
+		
+		Autor autor = autorRepository.findById(2L).orElse(null);
+		
+		Livro livro = new Livro();
+		livro.setDataPublicacao(datapublicacao);
+		livro.setGenero(GeneroLivro.FANTASIA);
+		livro.setIsbn("89YYYY67");
+		livro.setPreco(100.0);
+		livro.setTitulo("Meu primeiro Livro");
+		livro.setAutor(autor);
+		
+		livroRepository.save(livro);
+		
+		
+	}
+	
+	@Test
+	public void testSalvarLivroComNovoAutor() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2014, Calendar.AUGUST, 17);
+		Date datapublicacao = cal.getTime();
+		Calendar cal2 = Calendar.getInstance();
+		cal2.set(1900, Calendar.JANUARY, 9);
+		Date datanacimento = cal2.getTime();
+		
+		Autor autor = new Autor();
+		autor.setData_nascimento(datanacimento);
+		autor.setNome("JoseMaria");
+		autor.setNacionalidade("Portuguesa");
+		autor = autorRepository.save(autor);
+		
+		Livro livro = new Livro();
+		livro.setDataPublicacao(datapublicacao);
+		livro.setGenero(GeneroLivro.FICCAO);
+		livro.setIsbn("89YYYY00");
+		livro.setPreco(100.0);
+		livro.setTitulo("Meu Segundo Livro");
+		livro.setAutor(autor);
+		
+		livroRepository.save(livro);
+		
+		
+	}
+}
